a92b92cff8a404dcf10be4a7b4109da8d9e900bd
ruben <ruben@DESKTOP-4P3AAFU>
2022-11-17 11:05:07 +0100

    Creacion de los constructores

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -6,5 +6,34 @@ public class Reina {
 
 	private Color color;
 	private Posicion posicion;
+	public Reina() {
+		setColor(Color.BLANCO);
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	private void setColor(Color color) {
+
+		if (color == Color.BLANCO) {
+			this.posicion = new Posicion(1, 'd');
+		}
+		if (color == Color.NEGRO) {
+			this.posicion = new Posicion(8, 'd');
+		}
+		this.color = color;
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	private void setPosicion(Posicion posicion) {
+		if (posicion == null) {
+			throw new NullPointerException("Has introducido una posición nula.");
+		}
+		this.posicion = posicion;
+	}
 
 }
