a8b4d7542353472c5b108d3fc4371499dcebcb50
ruben <ruben@DESKTOP-4P3AAFU>
2022-11-18 09:27:52 +0100

    Creación del método mostrarMenu

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -9,6 +9,12 @@ public class Consola {
 	public Consola() {
 
 	}
+	
+	public static void mostrarMenu() {
+		System.out.println("1. Crear reina por defecto.");
+		System.out.println("2. Crear reina eligiendo el color.");
+		System.out.println("3. Mover.");
+		System.out.println("4. Salir.");
 
-
+	}
 }
